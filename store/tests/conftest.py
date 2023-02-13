1ee2d87254cca78497d47a968bb2dd0b22e8e917
BingBong <angusroymcdonald@gmail.com>
2023-02-13 21:06:19 +1100

    Implemented fixtures for user auth

--- store/tests/conftest.py
+++ store/tests/conftest.py
@@ -1,6 +1,22 @@
+"""
+By using this fixture in a PyTest test case, you can easily make API requests
+and test the behavior of your Django application's APIs. The advantage of using 
+fixtures is that the setup logic (in this case, creating an instance of APIClient) 
+is executed only once per test session, and the returned value is passed as an 
+argument to the test function that uses the fixture. This makes the code more readable,
+and helps you avoid repeating setup logic in multiple tests.
+"""
+
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 import pytest
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+@pytest.fixture
+def authenticate(api_client):
+    def do_authenticate(is_staff=False):
+        return api_client.force_authenticate(user=User(is_staff=is_staff))
+    return do_authenticate
--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -19,32 +19,30 @@ class TestCreateCollection:
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-    def test_if_user_is_not_admin_returns_403(self, api_client, create_collection): # 403 Forbidden
+    def test_if_user_is_not_admin_returns_403(self, create_collection, authenticate): # 403 Forbidden
         # Arrange
+        authenticate()
         # Act
-        api_client.force_authenticate(user={})
         response = create_collection({'title': 'a'})
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
-    def test_if_data_is_invalid_returns_400(self, api_client, create_collection): # 400 Bad Request
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_collection): # 400 Bad Request
         # Arrange
+        authenticate(is_staff=True)
         # Act
-        api_client.force_authenticate(user=User(is_staff=True))
         response = create_collection({'title': ''})
-
         # Assert
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None 
 
     
-    def test_if_data_is_valid_returns_201(self, api_client, create_collection):
+    def test_if_data_is_valid_returns_201(self, authenticate, create_collection):
         # Arrange
+        authenticate(is_staff=True)
         # Act
-        api_client.force_authenticate(user=User(is_staff=True))
         response = create_collection({'title': 'a'})
-
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
