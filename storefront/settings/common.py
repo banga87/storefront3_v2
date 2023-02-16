f807ce7366c6756ae51a4d3e14a9fe7b732544f2
BingBong <angusroymcdonald@gmail.com>
2023-02-17 09:55:13 +1100

    Updated production settings: redis, postgres, smtp

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -162,33 +162,18 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 2525
+
 DEFAULT_FROM_EMAIL = 'from@moshbuy.com'
 
 ADMINS = [
     ('Mosh', 'admin@moshbuy.com')
 ]
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_BEAT_SCHEDULE = {
     'notify_customers': {
         'task': 'playground.tasks.notify_customers',
         'schedule': 5.0,
         'args': ['Hello World'],
-    }
-}
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
     }
 }
 
--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,4 +1,5 @@
 from .common import *
+import dj_database_url
 import os
 
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -7,3 +8,26 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 
 ALLOWED_HOSTS = ['https://mosh-django-ecommerce.herokuapp.com/']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+REDIS_URL = os.environ['REDIS_URL']
+
+CELERY_BROKER_URL = REDIS_URL
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": REDIS_URL,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
+EMAIL_HOST_USER = os.environ['MAILGUN_SMTP_LOGIN']
+EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
+EMAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
