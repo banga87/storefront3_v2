763b2a99715195fd26671c903a7073eaccb4bb21
BingBong <angusroymcdonald@gmail.com>
2023-02-16 12:29:26 +1100

    Adjusted settings file for prod and dev environments.

---
+++ storefront/settings/dev.py
@@ -0,0 +1,18 @@
+from .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront3',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
